3fbf6b25443621e2bd9dc07742e984ce130e8983
Juan Orozco <aegoncold2002@gmail.com>
2023-06-06 10:37:56 -0600

    alter

--- src/components/Side-bar.vue
+++ src/components/Side-bar.vue
@@ -3,49 +3,49 @@
         <ul class="nav flex-column">
             <li class="nav-item">
                 <a class="nav-link" href="#">
-                    <img src="/assets/home.png" alt="Inicio" class="sidebar-icon">
+                    <img src="../assets/home.svg" alt="Inicio" class="sidebar-icon">
                     <span class="sidebar-title">Inicio</span>
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">
-                    <img src="/assets/micuenta.png" alt="Mi cuenta" class="sidebar-icon">
+                    <img src="../assets/micuenta.png" alt="Mi cuenta" class="sidebar-icon">
                     <span class="sidebar-title">Mi cuenta</span>
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">
-                    <img src="/assets/articles.png" alt="Ver articulos" class="sidebar-icon">
+                    <img src="../assets/verarticulos.png" alt="Ver articulos" class="sidebar-icon">
                     <span class="sidebar-title">Ver articulos</span>
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">
-                    <img src="/assets/inventory-in.png" alt="Entradas de inventario" class="sidebar-icon">
+                    <img src="../assets/entradainventario.png" alt="Entradas de inventario" class="sidebar-icon">
                     <span class="sidebar-title">Entradas de inventario</span>
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">
-                    <img src="/assets/inventory-out.png" alt="Salidas de Inventario" class="sidebar-icon">
+                    <img src="../assets/salidainventario.png" alt="Salidas de Inventario" class="sidebar-icon">
                     <span class="sidebar-title">Salidas de Inventario</span>
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">
-                    <img src="/assets/users.png" alt="Gestionar usuarios" class="sidebar-icon">
+                    <img src="../assets/gestionarusuarios.png" alt="Gestionar usuarios" class="sidebar-icon">
                     <span class="sidebar-title">Gestionar usuarios</span>
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">
-                    <img src="/assets/movements.png" alt="Registro de movimientos" class="sidebar-icon">
+                    <img src="../assets/registro.png" alt="Registro de movimientos" class="sidebar-icon">
                     <span class="sidebar-title">Registro de movimientos</span>
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">
-                    <img src="/assets/logout.png" alt="Salir" class="sidebar-icon">
+                    <img src="../assets/salir.png" alt="Salir" class="sidebar-icon">
                     <span class="sidebar-title">Salir</span>
                 </a>
             </li>
