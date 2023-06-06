27b9444469475571a919f8525ef496313293773b
Juan Orozco <aegoncold2002@gmail.com>
2023-06-06 10:35:03 -0600

    chg

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
+<template>
+  <div>
+    <sidebar></sidebar>
+  </div>
+</template>
+
 
 <script>
+import sidebar from './components/Side-bar.vue';
+
 export default {
   name: 'App',
   components: {
+    sidebar
   }
 }
 </script>
