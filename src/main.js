13a666b4b420573479b2fc2536b20e4f02d83cd5
Juan Orozco <aegoncold2002@gmail.com>
2023-06-06 10:16:19 -0600

    estrucutura inicial

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import './styles/custom-bootstrap.scss'
 
 createApp(App).mount('#app')
