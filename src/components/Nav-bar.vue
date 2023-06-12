46f9826f6b023fb5e5319f7fa676164254a33a2e
Andres Lopez Vargas <andresisaac.lopez@ucr.ac.cr>
2023-06-12 14:34:03 -0600

    Se modifica el sidebar Se instalan las dependencias de Bootstrap Icons

--- src/components/Nav-bar.vue
+++ src/components/Nav-bar.vue
@@ -1,57 +1,66 @@
 <template>
-    <nav class="navbar navbar-expand-lg">
-      <div class="logo-container">
-        <img id="logotelas" src="../assets/logo_telas.jpg" />
-      </div>
-      <div class="logo-usuario-container">
-        <svg
-          id="logousuario"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="currentColor"
-          class="bi bi-person"
-          viewBox="0 0 16 16"
-        >
-          <path
-            d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"
-          />
-        </svg>
-      </div>
-    </nav>
-  </template>
-  
-  <style lang="scss">
-  .navbar {
-    background-color: #15386e;
-    color: white;
-    height: 110px;
-  
-    .container {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      padding-top: 20px;
+  <nav class="navbar navbar-expand-lg">
+    <div class="logo-container">
+      <img id="logotelas" src="../assets/logo_telas.jpg" @click="NavigateToAnotherPage('home')"/>
+    </div>
+    <div class="logo-usuario-container">
+      <i id="logousuario" class="bi bi-person"></i>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss">
+.navbar {
+  background-color: #15386e;
+  color: white;
+  height: 110px;
+
+  .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-top: 20px;
+  }
+
+  .logo-container {
+    margin-left: 10px;
+    margin-right: 10px;
+
+    #logotelas {
+      width: 130px;
+      height: 80px;
     }
-  
-    .logo-container {
-      margin-left: 10px;
-      margin-right: 10px;
-  
-      #logotelas {
-        width: 130px;
-        height: 80px;
-      }
+
+    #logotelas:hover {
+      cursor: pointer;
     }
-  
-    .logo-usuario-container {
-      margin-top: 10px;
-      margin-right: 10px;
-      margin-left: auto;
-  
-      #logousuario {
-        width: 90px;
-        height: 80px;
-      }
+
+  }
+
+  .logo-usuario-container {
+    margin-top: 10px;
+    margin-right: 10px;
+    margin-left: auto;
+
+    #logousuario {
+      width: 90px;
+      height: 80px;
+      font-size: 70px;
+    }
+
+    #logousuario:hover {
+      cursor: pointer;
     }
   }
-  </style>
-  
+}
+</style>
+
+<script>
+export default {
+  methods: {
+    NavigateToAnotherPage(page) {
+      this.$router.push("/" + page);
+    },
+  },
+};
+</script>
