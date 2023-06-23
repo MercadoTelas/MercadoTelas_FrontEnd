2ece87281052e2223a9d3a46a9791c249c476901
Carlos Daniel Vargas Varela <carlos.vargasvarela@ucr.ac.cr>
2023-06-22 23:41:50 -0600

    Se finaliza funcionalidad de visualizar artículo y las entradas en inventario.

--- src/components/Transactional/Inventory-Entry.vue
+++ src/components/Transactional/Inventory-Entry.vue
@@ -1,91 +1,234 @@
 <template>
   <div class="form-container">
-    <div class="form-title col-sm-12 mx-auto">
-    </div>
-    <form class="form container col-sm-8">
+    <form class="form container" @submit.prevent="saveTransaction">
       <div class="row">
         <div class="col-md-6">
           <div class="form-group">
-            <label for="codigoArticulo" class="form-label">
+            <label for="itemID" class="form-label">
               <div class="blue-box">
                 <div class="label-text">Código del artículo</div>
               </div>
             </label>
-            <input v-model="articleId" type="text" class="form-control" id="codigoArticulo"
-              placeholder="Ingrese el código del artículo...">
+            <input v-model="itemID" type="text" class="form-control" id="itemID"
+                   placeholder="Ingrese el código del artículo...">
           </div>
-
           <div class="form-group">
-            <label for="cantidadIngresar" class="form-label">
+            <label for="entryAmount" class="form-label">
               <div class="blue-box">
                 <div class="label-text">Cantidad a ingresar</div>
               </div>
             </label>
-            <input v-model="amount" type="number" class="form-control" id="cantidadIngresar"
-              placeholder="Digite la cantidad a ingresar...">
-          </div>
-
-          <div class="form-group">
-            <label for="bodega" class="form-label">
-              <div class="blue-box">
-                <div class="label-text">Seleccione la bodega destino</div>
-              </div>
-            </label>
-            <select v-model="destinyWarehouse" class="form-select" id="bodega">
-              <option v-for="bodega in bodegas" :key="bodega">{{ bodega }}</option>
-            </select>
+            <input v-model="amount" type="number" class="form-control" id="entryAmount"
+                   placeholder="Digite la cantidad a ingresar...">
           </div>
         </div>
-
         <div class="col-sm-6">
           <div class="row">
             <div class="col-sm-12 text-center">
               <div class="form-group">
-                <button @click.prevent="addArticle" type="submit" class="btn btn-primary">Agregar artículo</button>
+                <button class="btn btn-primary" @click.prevent="insertItem">Agregar artículo</button>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </form>
 
-    <table class="table table-responsive">
-      <thead>
+      <table class="table table-responsive">
+        <thead>
         <tr>
           <th>Código</th>
           <th>Nombre artículo</th>
-          <th>Cantidad formato de entrada</th>
+          <th>Cantidad en unidades de almacenamiento</th>
           <th>Factor de Conversión</th>
-          <th>Cantidad formato Salida</th>
-          <th>Bodega</th>
+          <th>Cantidad en unidades de venta</th>
           <th></th>
         </tr>
-      </thead>
-      <tbody>
+        </thead>
+        <tbody>
         <tr v-for="(item, index) in tableData" :key="index">
-          <td>{{ item.codigo }}</td>
-          <td>{{ item.nombre }}</td>
-          <td>{{ item.cantidadEntrada }}</td>
-          <td>{{ item.factorConversion }}</td>
-          <td>{{ item.cantidadSalida }}</td>
-          <td>{{ item.bodega }}</td>
-          <td><button @click="removeItem(index)" class="btn btn-danger">Eliminar</button></td>
+          <td>{{ item.item_id }}</td>
+          <td>{{ item.name }}</td>
+          <td>{{ item.storing_format_units }} {{ item.storing_unit_format_name }}</td>
+          <td>
+            <input v-model="item.conversion_factor" type="number" class="form-control">
+          </td>
+          <td>{{ item.transferring_format_units }} {{ item.transferring_unit_format_name }}</td>
+          <td>
+            <button @click="removeItem(index)" class="btn btn-danger">Eliminar</button>
+          </td>
         </tr>
-      </tbody>
-    </table>
+        </tbody>
+      </table>
 
-    <div class="row">
-      <div class="col-sm-12 text-center">
-        <div class="form-group">
-          <button @click.prevent="realizarTransaccion" type="submit" class="btn btn-primary">Realizar transacción</button>
-          <button @click.prevent="cancelarTransaccion" class="btn btn-danger">Cancelar</button>
+      <div class="row">
+        <div class="col-sm-12 text-center">
+          <div class="form-group">
+            <button type="submit" class="btn btn-primary">Realizar transacción</button>
+            <button @click.prevent="cancel" class="btn btn-secondary">Cancelar</button>
+          </div>
         </div>
       </div>
-    </div>
+    </form>
   </div>
 </template>
+<script>
+import axios from "axios";
+import {API_URL} from "@/config";
+import Swal from "sweetalert2";
 
-  
+export default {
+  name: "EntradaInventario",
+  data() {
+    return {
+      itemID: '',
+      amount: 0,
+      inventory_item: [],
+      tableData: [],
+      inventory: {
+        id: '',
+        inventory_items: [],
+      }
+    };
+  },
+  mounted() {
+    this.$state.navbarTitle = 'Entradas de inventario';
+  },
+  watch: {
+    tableData: {
+      deep: true,
+      handler(newTableData) {
+        // Recalcular los valores de transferring_format_units para cada elemento en tableData
+        newTableData.forEach(item => {
+          item.transferring_format_units = item.storing_format_units * item.conversion_factor;
+        });
+      }
+    }
+  },
+  methods: {
+    cancel() {
+      Swal.fire({
+        title: "¿Estás seguro?",
+        text: "Se eliminarán todos los artículos agregados. Esta acción no se puede deshacer.",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sí, cancelar",
+        cancelButtonText: "No, volver",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          // Reinicia los datos de la tabla o realiza alguna acción adicional
+          this.tableData = [];
+          Swal.fire("Cancelado", "La transacción ha sido cancelada.", "success");
+        }
+      });
+    },
+    saveTransaction() {
+      if (this.tableData.length === 0) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Debe agregar al menos un artículo',
+        });
+        return;
+      }
+      for (let item of this.tableData) {
+        const inventoryItem = {
+          id: item.id,
+          item_id: item.item_id,
+          storing_format_units: item.storing_format_units,
+          transferring_format_units: item.transferring_format_units,
+          inventory_id: item.inventory_id,
+          created_at: item.created_at,
+          updated_at: item.updated_at,
+          name: item.name,
+          brand: item.brand,
+          design: item.design,
+          subcategory: item.subcategory,
+          category: item.category,
+          conversion_factor: item.conversion_factor,
+          storing_unit_format_name: item.storing_unit_format_name,
+          transferring_unit_format_name: item.transferring_unit_format_name,
+          warehouse: item.warehouse
+        };
+        this.inventory.inventory_items.push(inventoryItem);
+      }
+      console.log(this.inventory);
+      axios.post(`${API_URL}/inventories/insert_items`, this.inventory)
+          .then(response => {
+            Swal.fire({
+              icon: 'success',
+              title: 'Transacción realizada',
+              text: 'La transacción se ha realizado con éxito',
+            });
+            console.log(response);
+            this.tableData = [];
+          })
+          .catch(error => {
+            Swal.fire({
+              icon: 'error',
+              title: 'Error',
+              text: 'No se pudo realizar la transacción',
+            });
+            console.log(error);
+          });
+    }
+    ,
+    insertItem() {
+      if (this.itemID === '') {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Debe ingresar el código del artículo',
+        });
+        return;
+      }
+      // Realiza la llamada a la API para obtener los datos del artículo
+      axios.get(`${API_URL}/inventory_items/${this.itemID}`)
+          .then(response => {
+            // Procesa la respuesta de la API
+            this.inventory_item = response.data;
+            // Calcula la cantidad de unidades de almacenamiento
+            this.inventory_item.storing_format_units = this.amount;
+            // Calcula la cantidad de unidades de venta
+            this.inventory_item.transferring_format_units = this.amount * this.inventory_item.conversion_factor;
+            // Agrega el nuevo artículo a la tabla
+            this.tableData.push(this.inventory_item);
+          })
+          .catch(error => {
+            Swal.fire({
+              icon: 'error',
+              title: 'Error',
+              text: 'No se encontró el artículo',
+            });
+            console.log(error);
+          });
+    }
+    ,
+    removeItem(index) {
+      // Elimina el artículo de la tabla según el índice proporcionado
+      this.tableData.splice(index, 1);
+    }
+    ,
+
+    realizarTransaccion() {
+      // Realiza la transacción de los datos en la tabla enviando la información a la API
+      axios.post('/api/transacciones', this.tableData)
+          .then(response => {
+            // Procesa la respuesta de la transacción exitosa
+            console.log(response.data);
+            // Reinicia los datos de la tabla o realiza alguna acción adicional
+            this.tableData = [];
+          })
+          .catch(error => {
+            console.log(error);
+          });
+    }
+    ,
+  },
+};
+</script>
+
 <style lang="scss">
 .table-responsive {
   overflow-x: auto;
@@ -162,71 +305,4 @@
   }
 }
 </style>
-  
-<script>
-import axios from "axios";
-
-export default {
-  name: "EntradaInventario",
-  data() {
-    return {
-      bodegas: [1, 2, 3, 4, 5],
-      articleId: '',
-      amount: 0,
-      destinyWarehouse: 0,
-      tableData: [],
-    };
-  },
-  mounted() {
-    this.$state.navbarTitle = 'Entradas de inventario';
-  },
-  methods: {
-    methods: {
-      addArticle() {
-        // Realiza la llamada a la API para obtener los datos del artículo
-        axios.get(`/api/articulos/${this.articleId}`)
-          .then(response => {
-            const articleData = response.data;
-            const newItem = {
-              codigo: articleData.codigo,
-              nombre: articleData.nombre,
-              cantidadEntrada: this.amount,
-              factorConversion: articleData.factorConversion,
-              cantidadSalida: this.amount * articleData.factorConversion,
-              bodega: this.destinyWarehouse,
-            };
-
-            // Agrega el nuevo artículo a la tabla
-            this.tableData.push(newItem);
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      },
-      removeItem(index) {
-        // Elimina el artículo de la tabla según el índice proporcionado
-        this.tableData.splice(index, 1);
-      },
-      realizarTransaccion() {
-        // Realiza la transacción de los datos en la tabla enviando la información a la API
-        axios.post('/api/transacciones', this.tableData)
-          .then(response => {
-            // Procesa la respuesta de la transacción exitosa
-            console.log(response.data);
-            // Reinicia los datos de la tabla o realiza alguna acción adicional
-            this.tableData = [];
-          })
-          .catch(error => {
-            console.log(error);
-          });
-      },
-      cancelarTransaccion() {
-        // Realiza la lógica de cancelación de la transacción
-        // Reinicia los datos de la tabla o realiza alguna acción adicional
-        this.tableData = [];
-      },
-    },
-  },
-};
-</script>
   
