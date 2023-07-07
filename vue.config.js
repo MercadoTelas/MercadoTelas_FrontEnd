89dbe57f1ff01387954c825b45d6b58ee32aefce
Carlos Daniel Vargas Varela <carlos.vargasvarela@ucr.ac.cr>
2023-07-06 18:20:49 -0600

    Cambios para hostear en GHubPages

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/' + process.env.CI_PROJECT_NAME + '/'
-      : '/',
+  publicPath: process.env.NODE_ENV === "production" ? "/MercadoTelas_FrontEnd/" : "/",
   transpileDependencies: true,
   css: {
     loaderOptions: {
