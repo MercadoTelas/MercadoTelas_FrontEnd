d22a6ba531d5261ae224e4bb663214d087f0c680
Carlos Daniel Vargas Varela <carlos.vargasvarela@ucr.ac.cr>
2023-07-08 17:26:59 -0600

    Trabajo conexión con API, vista upsertArticle

--- src/components/Adds/AddArticle.vue
+++ src/components/Adds/AddArticle.vue
@@ -4,120 +4,121 @@
       <div class="table-responsive">
         <table class="table table-bordered table-secondary">
           <tbody>
-            <tr>
-              <td class="table-label">Código:</td>
-              <td class="table-input" colspan="5">
-                <div class="input-group">
-                  <span class="input-group-text"><i class="bi bi-123"></i></span>
-                  <input type="text" id="code" class="form-control" :value="id" :readonly="isReadOnly">
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td class="table-label">Nombre:</td>
-              <td class="table-input" colspan="5">
-                <div class="input-group">
-                  <span class="input-group-text"><i class="bi bi-person-badge-fill"></i></span>
-                  <input type="text" id="name" class="form-control" :value="name" :readonly="isReadOnly">
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td class="table-label">Stock Mínimo:</td>
-              <td class="table-input">
-                <div class="input-group">
-                  <span class="input-group-text"><i class="bi bi-dash-square-fill"></i></span>
-                  <input type="number" id="minimal_stock" class="form-control" :value="minimal_stock"
-                    :readonly="isReadOnly">
-                </div>
-              </td>
-              <td class="table-label">Factor de Conversión:</td>
-              <td class="table-input">
-                <div class="input-group">
-                  <span class="input-group-text"><i class="bi bi-input-cursor"></i></span>
-                  <input type="number" id="conversion_factor" class="form-control" :value="conversion_factor"
-                    :readonly="isReadOnly">
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td class="table-label">Unidades de Almacenamiento:</td>
-              <td class="table-input">
-                <div class="input-group">
-                  <span class="input-group-text"><i class="bi bi-postcard"></i></span>
-                  <input type="text" id="storing_format_units_name" class="form-control"
-                    :value="storing_format_units_name" :readonly="isReadOnly">
-                </div>
-              </td>
-              <td class="table-label">Unidades de Venta:</td>
-              <td class="table-input">
-                <div class="input-group">
-                  <span class="input-group-text"><i class="bi bi-postcard"></i></span>
-                  <input type="text" id="transferring_format_units_name" class="form-control"
-                    :value="transferring_format_units_name" :readonly="isReadOnly">
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td class="table-label">Categoría:</td>
-              <td class="table-input" colspan="5">
-                <div class="input-group">
-                  <span class="input-group-text"><i class="bi bi-tag-fill"></i></span>
-                  <select id="category" class="form-select" v-model="filter.category" @change="resetSubcategory"
-                    :disabled="isReadOnly">
-                    <option value="" disabled>Seleccionar categoría</option>
-                    <option v-for="category in categories" :value="category.name" :key="category.name">{{ category.name }}
-                    </option>
-                  </select>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td class="table-label">Subcategoría:</td>
-              <td class="table-input" colspan="5">
-                <div class="input-group">
-                  <span class="input-group-text"><i class="bi bi-tags"></i></span>
-                  <select id="subcategory" class="form-select" v-model="subcategory_id" :disabled="isReadOnly">
-                    <option value="" disabled selected>Seleccionar subcategoría</option>
-                    <option v-for="subcategory in filteredSubcategories" :value="subcategory.id" :key="subcategory.name">
-                      {{ subcategory.name }}</option>
-                  </select>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td class="table-label">Diseño:</td>
-              <td class="table-input" colspan="5">
-                <div class="input-group">
-                  <span class="input-group-text"><i class="bi bi-x-diamond-fill"></i></span>
-                  <select id="design" class="form-select" v-model="design_id" :disabled="isReadOnly">
-                    <option value="" selected disabled>Seleccionar diseño</option>
-                    <option v-for="design in designs" :value="design.id" :key="design.id">{{ design.name }}</option>
-                  </select>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td class="table-label">Marca:</td>
-              <td class="table-input" colspan="5">
-                <div class="input-group">
-                  <span class="input-group-text"><i class="bi bi-medium"></i></span>
-                  <select id="brand" class="form-select" v-model="brand_id" :disabled="isReadOnly">
-                    <option value="" selected disabled>Seleccionar marca</option>
-                    <option v-for="brand in brands" :value="brand.id" :key="brand.id">{{ brand.name }}</option>
-                  </select>
-                </div>
-              </td>
-            </tr>
-            <tr>
-              <td class="table-label">Imagen:</td>
-              <td class="table-input" colspan="5">
-                <div class="input-group">
-                  <span class="input-group-text"><i class="bi bi-card-image"></i></span>
-                  <input type="file" id="image" class="form-control" accept="image/*" @change="handleImageChange">
-                </div>
-              </td>
-            </tr>
+          <tr>
+            <td class="table-label">Código:</td>
+            <td class="table-input" colspan="5">
+              <div class="input-group">
+                <span class="input-group-text"><i class="bi bi-123"></i></span>
+                <input type="text" id="code" class="form-control" :value="id" :readonly="isReadOnly">
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="table-label">Nombre:</td>
+            <td class="table-input" colspan="5">
+              <div class="input-group">
+                <span class="input-group-text"><i class="bi bi-person-badge-fill"></i></span>
+                <input type="text" id="name" class="form-control" :value="name" :readonly="isReadOnly">
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="table-label">Stock Mínimo:</td>
+            <td class="table-input">
+              <div class="input-group">
+                <span class="input-group-text"><i class="bi bi-dash-square-fill"></i></span>
+                <input type="number" id="minimal_stock" class="form-control" :value="minimal_stock"
+                       :readonly="isReadOnly">
+              </div>
+            </td>
+            <td class="table-label">Factor de Conversión:</td>
+            <td class="table-input">
+              <div class="input-group">
+                <span class="input-group-text"><i class="bi bi-input-cursor"></i></span>
+                <input type="number" id="conversion_factor" class="form-control" :value="conversion_factor"
+                       :readonly="isReadOnly">
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="table-label">Unidades de Almacenamiento:</td>
+            <td class="table-input">
+              <div class="input-group">
+                <span class="input-group-text"><i class="bi bi-postcard"></i></span>
+                <input type="text" id="storing_format_units_name" class="form-control"
+                       :value="storing_format_units_name" :readonly="isReadOnly">
+              </div>
+            </td>
+            <td class="table-label">Unidades de Venta:</td>
+            <td class="table-input">
+              <div class="input-group">
+                <span class="input-group-text"><i class="bi bi-postcard"></i></span>
+                <input type="text" id="transferring_format_units_name" class="form-control"
+                       :value="transferring_format_units_name" :readonly="isReadOnly">
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="table-label">Categoría:</td>
+            <td class="table-input" colspan="5">
+              <div class="input-group">
+                <span class="input-group-text"><i class="bi bi-tag-fill"></i></span>
+                <select id="category" class="form-select" v-model="filter.category" @change="resetSubcategory"
+                        :disabled="isReadOnly">
+                  <option value="" disabled>Seleccionar categoría</option>
+                  <option v-for="category in categories" :value="category.name" :key="category.name">{{ category.name }}
+                  </option>
+                </select>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="table-label">Subcategoría:</td>
+            <td class="table-input" colspan="5">
+              <div class="input-group">
+                <span class="input-group-text"><i class="bi bi-tags"></i></span>
+                <select id="subcategory" class="form-select" v-model="subcategory_id" :disabled="isReadOnly">
+                  <option value="" disabled selected>Seleccionar subcategoría</option>
+                  <option v-for="subcategory in filteredSubcategories" :value="subcategory.id" :key="subcategory.name">
+                    {{ subcategory.name }}
+                  </option>
+                </select>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="table-label">Diseño:</td>
+            <td class="table-input" colspan="5">
+              <div class="input-group">
+                <span class="input-group-text"><i class="bi bi-x-diamond-fill"></i></span>
+                <select id="design" class="form-select" v-model="design_id" :disabled="isReadOnly">
+                  <option value="" selected disabled>Seleccionar diseño</option>
+                  <option v-for="design in designs" :value="design.id" :key="design.id">{{ design.name }}</option>
+                </select>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="table-label">Marca:</td>
+            <td class="table-input" colspan="5">
+              <div class="input-group">
+                <span class="input-group-text"><i class="bi bi-medium"></i></span>
+                <select id="brand" class="form-select" v-model="brand_id" :disabled="isReadOnly">
+                  <option value="" selected disabled>Seleccionar marca</option>
+                  <option v-for="brand in brands" :value="brand.id" :key="brand.id">{{ brand.name }}</option>
+                </select>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td class="table-label">Imagen:</td>
+            <td class="table-input" colspan="5">
+              <div class="input-group">
+                <span class="input-group-text"><i class="bi bi-card-image"></i></span>
+                <input type="file" id="image" class="form-control" accept="image/*" @change="handleImageChange">
+              </div>
+            </td>
+          </tr>
           </tbody>
         </table>
       </div>
@@ -131,11 +132,10 @@
 
 <script>
 import axios from 'axios';
-import Swal from 'sweetalert2';
-import { toast } from 'vue3-toastify';
+import {toast} from 'vue3-toastify';
 import 'vue3-toastify/dist/index.css';
 
-import { API_URL } from "@/config";
+import {API_URL} from "@/config";
 
 export default {
   data() {
@@ -156,7 +156,8 @@ export default {
       designs: [],
       brands: [],
       image: null,
-      isReadOnly: false
+      isReadOnly: false,
+      mode: ''
     };
   },
   computed: {
@@ -190,37 +191,49 @@ export default {
     },
     fetchNewItemData() {
       this.$state.navbarTitle = 'Agregar Nuevo Artículo';
+      this.mode = 'new';
       axios.get(`${API_URL}/new_item`)
-        .then(response => {
-          this.categories = response.data.categories;
-          this.designs = response.data.designs;
-          this.brands = response.data.brands;
-          console.log(response);
-        })
-        .catch(error => {
-          console.log(error);
-        });
+          .then(response => {
+            this.categories = response.data.categories;
+            this.designs = response.data.designs;
+            this.brands = response.data.brands;
+            console.log(response);
+          })
+          .catch(error => {
+            console.log(error);
+          });
     },
     fetchItemData(itemId) {
+      this.mode = 'edit';
       this.$state.navbarTitle = 'Editar Artículo';
+      axios.get(`${API_URL}/new_item`)
+          .then(response => {
+            this.categories = response.data.categories;
+            this.designs = response.data.designs;
+            this.brands = response.data.brands;
+            console.log(response);
+          })
+          .catch(error => {
+            console.log(error);
+          });
       axios.get(`${API_URL}/items/${itemId}`)
-        .then(response => {
-          const itemData = response.data;
-          this.id = itemData.id;
-          this.name = itemData.name;
-          this.minimal_stock = itemData.minimal_stock;
-          this.storing_format_units_name = itemData.storing_format_units_name;
-          this.transferring_format_units_name = itemData.transferring_format_units_name;
-          this.conversion_factor = itemData.conversion_factor;
-          this.subcategory_id = itemData.subcategory_id;
-          this.design_id = itemData.design_id;
-          this.brand_id = itemData.brand_id;
-          // También puedes realizar una solicitud adicional para obtener los datos de categoría, diseño y marca si es necesario.
-          console.log(response);
-        })
-        .catch(error => {
-          console.log(error);
-        });
+          .then(response => {
+            const itemData = response.data;
+            this.id = itemData.id;
+            this.name = itemData.name;
+            this.minimal_stock = itemData.minimal_stock;
+            this.storing_format_units_name = itemData.storing_format_units_name;
+            this.transferring_format_units_name = itemData.transferring_format_units_name;
+            this.conversion_factor = itemData.conversion_factor;
+            this.filter.category = itemData.category_name;
+            this.subcategory_id = itemData.subcategory_id;
+            this.design_id = itemData.design_id;
+            this.brand_id = itemData.brand_id;
+            console.log(response);
+          })
+          .catch(error => {
+            console.log(error);
+          });
     },
     onSubmit() {
       const item = {
@@ -235,32 +248,35 @@ export default {
         brand_id: this.brand_id,
         image: this.image
       };
-
-      const formData = new FormData();
-      for (const key in item) {
-        formData.append(key, item[key]);
+      if (this.mode === 'edit') {
+        this.updateItem(item);
+      } else {
+        this.createItem(item);
       }
-
-      console.log(formData); // Check the form data in the console
-
-      console.log(item);
-      axios.post(`${API_URL}/items`, item)
-        .then(response => {
-          // Mostrar Toast de éxito
-      toast.success('Artículo creado', {
-        autoClose: 2000, // Duración en milisegundos
-      });
-          console.log(response);
-        })
-        .catch(error => {
-          Swal.fire({
-            title: '¡Error!',
-            text: 'No se pudo crear el artículo',
-            icon: 'error',
-            confirmButtonText: 'Aceptar'
+    },
+    updateItem(item) {
+      axios.put(`${API_URL}/items/${item.id}`, item)
+          .then(response => {
+            console.log(response);
+            toast.success('Artículo actualizado exitosamente');
+            this.$router.push('/items');
+          })
+          .catch(error => {
+            console.log(error);
+            toast.error('Error al actualizar artículo');
           });
-          console.log(error);
-        });
+    },
+    createItem(item) {
+      axios.post(`${API_URL}/items`, item)
+          .then(response => {
+            console.log(response);
+            toast.success('Artículo creado exitosamente');
+            this.$router.push('/items');
+          })
+          .catch(error => {
+            console.log(error);
+            toast.error('Error al crear artículo');
+          });
     }
   }
 };
